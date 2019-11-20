99c8e8d8ec74c68a23dccc1a67d8569060f05c26
HYOJINPARK <wolfrun9@gmail.com>
2019-11-21 00:16:38 +0900

    Add files via upload

---
+++ etc/Criteria.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+__author__ = "Sachin Mehta"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Sachin Mehta"
+
+
+class CrossEntropyLoss2d(nn.Module):
+    '''
+    This file defines a cross entropy loss for 2D images
+    '''
+    def __init__(self, weight=None, ignore = None):
+        '''
+        :param weight: 1D weight vector to deal with the class-imbalance
+        '''
+
+        super().__init__()
+        if int(torch.__version__[2]) < 4:
+            self.loss = nn.NLLLoss2d(weight, ignore_index=ignore)
+        else:
+            self.loss = nn.NLLLoss(weight, ignore_index=ignore)
+
+    def forward(self, outputs, targets):
+        # print(torch.unique(targets))
+        return self.loss(F.log_softmax(outputs, 1), targets)
---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .extremeC3_small import *
+from .extremeC3 import *
+
+
